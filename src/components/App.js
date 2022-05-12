862b1024329ad6518554f383125f66d89c4e081b
darovero <drvelasquezr@outlook.com>
2022-05-12 14:42:43 -0500

    create github workflows 06

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ export const App = () => {
   return (
     <div className="app">
       <img src={hooray} alt="celebrate" />
-      <h1>Welcome to my website test from an AWS S3!</h1>
+      <h1>Welcome to my website from an AWS S3!</h1>
     </div>
   );
 };
